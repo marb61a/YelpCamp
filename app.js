51ba22e10ec8cd34931f30e94ef8f0203900a1fb
marb <marb61a@gmail.com>
2016-10-26 14:16:49 +0000

    added initial index routes and landing page

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var express = require("express"),
     mongoose = require("mongoose"),
     passport = require("passport")
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
 app.use(function(req, res, next) {
   res.locals.currentUser = req.user;
   next();
