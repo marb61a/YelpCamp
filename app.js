a5112a0f32a27a4a810a81852c23c117214d60d1
marb <marb61a@gmail.com>
2016-10-27 09:55:10 +0000

    added login view  and updated index routes

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ var express = require("express"),
     app = express,
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
-    passport = require("passport")
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require('method-override'),
+    User = require("./models/user")
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride('_method'));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 
 app.use(function(req, res, next) {
   res.locals.currentUser = req.user;
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,24 @@ var User = require('../models/user');
 
 router.get("/", function(req, res){
     res.render("landing");
-})
+});
+
+router.get("/register", function(req, res){
+    res.render("register");
+});
+
+// Register route
+router.post("/register", function(req, res){
+    var newUser = new User({
+        username : req.body.username
+    });
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
+    });
+});
