1cddcdf6f3dafeff012f47d72c5828d9ca345db4
marb <marb61a@gmail.com>
2016-10-31 10:30:04 +0000

    updated app js and added login routes to index.js

--- app.js
+++ app.js
@@ -5,21 +5,39 @@ var express = require("express"),
     passport = require("passport"),
     LocalStrategy = require("passport-local"),
     methodOverride = require('method-override'),
+    Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
     User = require("./models/user")
+    
+// Routes required
+var commentRoutes = require('./routes/comments'),
+    campgroundRoutes = require('./routes/campgrounds'),
+    indexRoutes = require('./routes/index');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride('_method'));
 
+// Passport Configuration
+app.use(require("express-session")({
+    secret: "",
+    resave: false,
+    saveUninitialized: false
+}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next) {
   res.locals.currentUser = req.user;
   next();
 });
+
+app.use(indexRoutes);
 
 app.listen(process.env.PORT || 3000, function(){
    console.log("The YelpCamp Server Has Started!");
--- routes/index.js
+++ routes/index.js
@@ -26,3 +26,15 @@ router.post("/register", function(req, res){
         });
     });
 });
+
+// Login form
+router.get("/login", function(req, res) {
+    res.render("login");
+});
+
+router.post("/login", passport.authenticate("local", 
+    {
+        successRedirect : "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res){
+    });
