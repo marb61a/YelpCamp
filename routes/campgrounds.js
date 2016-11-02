b690d5f85fbfbeae5a7570a8d1dc45318e202bd4
marb <marb61a@gmail.com>
2016-11-02 10:26:01 +0000

    added campground functions

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -90,4 +90,31 @@ router.delete('/:id', function(req, res){
     });
 });
 
+// Middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
+
+function checkCampgroundOwnership(req, res, next){
+    if(req.isAuthenticated()){
+        // Does the user own that campground
+        Campground.findById(req.params.id, function(err, foundCampground) {
+            if(err){
+                res.redirect("back");
+            } else {
+                if(foundCampground.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    res.redirect("back");    
+                }
+            }
+        });
+    } else {
+        res.redirect('back');
+    }
+}
+
 module.exports = router;
