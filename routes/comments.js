bf4feedb7bc8ff1714d77c52bb39ab85eb7a3e21
marb <marb61a@gmail.com>
2016-11-02 10:41:24 +0000

    added comments view file

--- routes/comments.js
+++ routes/comments.js
@@ -3,6 +3,19 @@ var router = express.Router({mergeParams: true});
 var Campground = require('../models/campground');
 var Comment = require('../models/comment');
 
+// New Comments
+router.get('/new', isLoggedIn, function(req, res){
+    // Find a campground by the ID
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render('comments/new', {campground: campground});
+        }
+    });
+});
+
+
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()) {
         return next();
